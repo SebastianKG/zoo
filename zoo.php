62819504734ef255d66c12d5b9e46e525cf2e5fe
Sebastian Kazenbroot-Guppy <sebastian.kg@gmail.com>
2013-11-17 13:53:08 -0800

    Removing Gridster support, updating SQL

--- zoo.php
+++ zoo.php
@@ -1,39 +1,15 @@
 <html>
 	<head>
-		<title>Zoo</title>
+		<title>ZOO // CS 304</title>
 		<meta name="description" content="Our cool zoo! Made by Sebastian Kazenbroot-Guppy, Norris Lee and Harlen Bains."
-		<link rel="stylesheet" type="text/css" href="http://gridster.net/dist/jquery.gridster.min.css">
-		<link rel="stylesheet" type="text/css" href="http://gridster.net/assets/css/style.css">
 		<link rel="stylesheet" type="text/css" href="main.css">
 	</head>
 	<body>
 		<div id="mycontainer">
-			<div class="gridster">
-			    <ul>
-			        <li data-row="1" data-col="1" data-sizex="1" data-sizey="1"></li>
-			        <li data-row="2" data-col="1" data-sizex="1" data-sizey="1"></li>
-			        <li data-row="3" data-col="1" data-sizex="1" data-sizey="1"></li>
-			 
-			        <li data-row="1" data-col="2" data-sizex="2" data-sizey="1"></li>
-			        <li data-row="2" data-col="2" data-sizex="2" data-sizey="2"></li>
-			 
-			        <li data-row="1" data-col="4" data-sizex="1" data-sizey="1"></li>
-			        <li data-row="2" data-col="4" data-sizex="2" data-sizey="1"></li>
-			        <li data-row="3" data-col="4" data-sizex="1" data-sizey="1"></li>
-			 
-			        <li data-row="1" data-col="5" data-sizex="1" data-sizey="1"></li>
-			        <li data-row="3" data-col="5" data-sizex="1" data-sizey="1"></li>
-			 
-			        <li data-row="1" data-col="6" data-sizex="1" data-sizey="1"></li>
-			        <li data-row="2" data-col="6" data-sizex="1" data-sizey="2"></li>
-			    </ul>
-			</div>
-
-			<button name="submitbtn" type="submit">Submit</button>
+			
 		</div>
 
 		<script type="text/javascript" src="http://gridster.net/assets/js/libs/jquery-1.7.2.min.js"></script>
-  		<script src="http://gridster.net/dist/jquery.gridster.js" type="text/javascript" charset="utf-8"></script>
   		<script src="application.js" type="text/javascript"></script>
 	</body>
 </html>
