10bb90fdf75f8a451692455cb7fcaea561b41ad3
Sebastian Kazenbroot-Guppy <sebastian.kg@gmail.com>
2013-11-20 19:38:34 -0800

    Added database-connection code in index.php

--- index.php
+++ index.php
@@ -9,13 +9,27 @@
 			<button type="button" name="logout">Log Out</btn>
 
 			<?php
-				$result = executePlainSQL("select * from zoo");
-				printAllZoos($result);
+				$success = True; //keep track of errors so it redirects the page only if there are no errors
+				$db_conn = OCILogon("ora_w8x7", "a67961045", "ug");
+
+				require ('functions.php');
+
+				if ($db_conn) {
+					$result = executePlainSQL("select * from zoo");
+					printAllZoos($result);
+				
+						//Commit to save changes...
+					OCILogoff($db_conn);
+				} else {
+					echo "cannot connect";
+					$e = OCI_Error(); // For OCILogon errors pass no handle
+					echo htmlentities($e['message']);
+				}
 			?>
 
 			<div id="log">ourLog</div>
 		</div>
-		
+
 		<script type="text/javascript" src="http://gridster.net/assets/js/libs/jquery-1.7.2.min.js"></script>
 		<script src="application.js" type="text/javascript"></script>
 	</body>
