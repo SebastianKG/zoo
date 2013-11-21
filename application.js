7faec32af3b2a2d8363b62445d2d6f3115f22c62
Sebastian Kazenbroot-Guppy <sebastian.kg@gmail.com>
2013-11-20 19:58:30 -0800

    Changed button javascript for both index and zoo

--- application.js
+++ application.js
@@ -1,8 +1,18 @@
-$( "button" ).click(function( event ) {
-	var myname = event.target.name;
-  	$( "#log" ).html( "clicked: " + myname );
-  	setCookie("zooname", myname, 1);
-  	window.location.replace("index.php");
+// with reference to http://www.w3schools.com/js/js_cookies.asp
+function setCookie(cookieName,value,exdays) {
+	var exdate=new Date();
+	exdate.setDate(exdate.getDate() + exdays);
+	var c_value = escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
+	document.cookie = cookieName + "=" + c_value;
+}
+
+$(function () {
+	$( "button" ).click(function( event ) {
+		var myname = event.target.name;
+	  	$( "#log" ).html( "clicked: " + myname );
+	  	setCookie("zooname", myname, 1);
+	  	window.location.replace("zoo.php");
+	});
 });
 
 $(function() {
@@ -12,10 +22,3 @@ $(function() {
 	});
 });
 
-// with reference to http://www.w3schools.com/js/js_cookies.asp
-function setCookie(cookieName,value,exdays) {
-	var exdate=new Date();
-	exdate.setDate(exdate.getDate() + exdays);
-	var c_value = escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
-	document.cookie = cookieName + "=" + c_value;
-}
