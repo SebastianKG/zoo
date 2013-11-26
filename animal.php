e4b24fc4edf48752748ea6e77e6d39ec0d62ce8c
Norris Lee <norrislee18@Norriss-MacBook-Air.local>
2013-11-25 19:31:03 -0800

    add buy item, update animal stats and cash feature

--- animal.php
+++ animal.php
@@ -27,31 +27,52 @@
 		// Connect Oracle...
 		if ($db_conn) {
             // Happy Meal, Durian, GameBoy, White Russian, Axe Bodyspray
+            // Hydration -25, Hygiene -25, Fullness +50, Happiness +50, Price 350
             if (array_key_exists('HappyMeal', $_POST)) {
-                $query = "update purchaseitem set amount=(select amount from purchaseitem where zooname='" . $zooname . "' and name='HappyMeal')-1 where zooname='" . $zooname ."' and name='HappyMeal'";
-                executePlainSQL($query);
+                $itemquery = "update purchaseitem set amount=(select amount from purchaseitem where zooname='" . $zooname . "' and name='HappyMeal')-1 where zooname='" . $zooname ."' and name='HappyMeal'";
+                $animalquery = "update purchaseanimal set hydration=(select max(case when hydration<0 then 0 else (select hydration-25 from purchaseanimal where zooname='" . $zooname . "' and name='" . $animalname . "') end) from purchaseanimal where name='" . $animalname . "' and zooname='" . $zooname . "'), hygiene=(select max(case when hygiene<0 then 0 else (select hygiene-25 from purchaseanimal where zooname='" . $zooname . "' and name='" . $animalname . "') end) from purchaseanimal where name='" . $animalname . "' and zooname='" . $zooname . "'), fullness=(select max(case when fullness<0 then 0 else (select fullness+50 from purchaseanimal where zooname='" . $zooname . "' and name='" . $animalname . "') end) from purchaseanimal where name='" . $animalname . "' and zooname='" . $zooname . "'), happiness=(select max(case when happiness<0 then 0 else (select happiness+50 from purchaseanimal where zooname='" . $zooname . "' and name='" . $animalname . "') end) from purchaseanimal where name='" . $animalname . "' and zooname='" . $zooname . "') where zooname='" . $zooname . "' and name='" . $animalname . "'";
+                $cashquery = "update zoo set cash=(select cash from zoo where name='" . $zooname . "')-350 where name='" . $zooname . "'";
+                executePlainSQL($itemquery);
+                executePlainSQL($animalquery);
+                executePlainSQL($cashquery);
                 OCICommit($db_conn);
-            }// Hydration -25, Hygiene -25, Fullness +50, Happiness +50
+            }// Hydration +5, Hygiene -20, Fullness +75, Happiness -10, Price 200
             if (array_key_exists('Durian', $_POST)) {
-                $query = "update purchaseitem set amount=(select amount from purchaseitem where zooname='" . $zooname . "' and name='Durian')-1 where zooname='" . $zooname ."' and name='Durian'";
-                executePlainSQL($query);
+                $itemquery = "update purchaseitem set amount=(select amount from purchaseitem where zooname='" . $zooname . "' and name='Durian')-1 where zooname='" . $zooname ."' and name='Durian'";
+                $animalquery = "update purchaseanimal set hydration=(select max(case when hydration<0 then 0 else (select hydration+5 from purchaseanimal where zooname='" . $zooname . "' and name='" . $animalname . "') end) from purchaseanimal where name='" . $animalname . "' and zooname='" . $zooname . "'), hygiene=(select max(case when hygiene<0 then 0 else (select hygiene-20 from purchaseanimal where zooname='" . $zooname . "' and name='" . $animalname . "') end) from purchaseanimal where name='" . $animalname . "' and zooname='" . $zooname . "'), fullness=(select max(case when fullness<0 then 0 else (select fullness+75 from purchaseanimal where zooname='" . $zooname . "' and name='" . $animalname . "') end) from purchaseanimal where name='" . $animalname . "' and zooname='" . $zooname . "'), happiness=(select max(case when happiness<0 then 0 else (select happiness-10 from purchaseanimal where zooname='" . $zooname . "' and name='" . $animalname . "') end) from purchaseanimal where name='" . $animalname . "' and zooname='" . $zooname . "') where zooname='" . $zooname . "' and name='" . $animalname . "'";
+                $cashquery = "update zoo set cash=(select cash from zoo where name='" . $zooname . "')-200 where name='" . $zooname . "'";
+                executePlainSQL($itemquery);
+                executePlainSQL($animalquery);
+                executePlainSQL($cashquery);
                 OCICommit($db_conn);
-            }// Hydration +5, Hygiene -20, Fullness +75, Happiness -10
+            }// Hydration -5, Hygiene -20, Fullness -5, Happiness +30, Price 500
             if (array_key_exists('GameBoy', $_POST)) {
-                $query = "update purchaseitem set amount=(select amount from purchaseitem where zooname='" . $zooname . "' and name='GameBoy')-1 where zooname='" . $zooname ."' and name='GameBoy'";
-                executePlainSQL($query);
+                $itemquery = "update purchaseitem set amount=(select amount from purchaseitem where zooname='" . $zooname . "' and name='GameBoy')-1 where zooname='" . $zooname ."' and name='GameBoy'";
+                $animalquery = "update purchaseanimal set hydration=(select max(case when hydration<0 then 0 else (select hydration-5 from purchaseanimal where zooname='" . $zooname . "' and name='" . $animalname . "') end) from purchaseanimal where name='" . $animalname . "' and zooname='" . $zooname . "'), hygiene=(select max(case when hygiene<0 then 0 else (select hygiene-20 from purchaseanimal where zooname='" . $zooname . "' and name='" . $animalname . "') end) from purchaseanimal where name='" . $animalname . "' and zooname='" . $zooname . "'), fullness=(select max(case when fullness<0 then 0 else (select fullness-5 from purchaseanimal where zooname='" . $zooname . "' and name='" . $animalname . "') end) from purchaseanimal where name='" . $animalname . "' and zooname='" . $zooname . "'), happiness=(select max(case when happiness<0 then 0 else (select happiness+30 from purchaseanimal where zooname='" . $zooname . "' and name='" . $animalname . "') end) from purchaseanimal where name='" . $animalname . "' and zooname='" . $zooname . "') where zooname='" . $zooname . "' and name='" . $animalname . "'";
+                $cashquery = "update zoo set cash=(select cash from zoo where name='" . $zooname . "')-500 where name='" . $zooname . "'";
+                executePlainSQL($itemquery);
+                executePlainSQL($animalquery);
+                executePlainSQL($cashquery);
                 OCICommit($db_conn);
-            }// Hydration -5, Hygiene -20, Fullness -5, Happiness +30
+            }// Hydration +20, Hygiene -20, Fullness -5, Happiness +20, Price 250
             if (array_key_exists('WhiteRussian', $_POST)) {
-                $query = "update purchaseitem set amount=(select amount from purchaseitem where zooname='" . $zooname . "' and name='WhiteRussian')-1 where zooname='" . $zooname ."' and name='WhiteRussian'";
-                executePlainSQL($query);
+                $itemquery = "update purchaseitem set amount=(select amount from purchaseitem where zooname='" . $zooname . "' and name='WhiteRussian')-1 where zooname='" . $zooname ."' and name='WhiteRussian'";
+                $animalquery = "update purchaseanimal set hydration=(select max(case when hydration<0 then 0 else (select hydration-25 from purchaseanimal where zooname='" . $zooname . "' and name='" . $animalname . "') end) from purchaseanimal where name='" . $animalname . "' and zooname='" . $zooname . "'), hygiene=(select max(case when hygiene<0 then 0 else (select hygiene-25 from purchaseanimal where zooname='" . $zooname . "' and name='" . $animalname . "') end) from purchaseanimal where name='" . $animalname . "' and zooname='" . $zooname . "'), fullness=(select max(case when fullness<0 then 0 else (select fullness+50 from purchaseanimal where zooname='" . $zooname . "' and name='" . $animalname . "') end) from purchaseanimal where name='" . $animalname . "' and zooname='" . $zooname . "'), happiness=(select max(case when happiness<0 then 0 else (select happiness+50 from purchaseanimal where zooname='" . $zooname . "' and name='" . $animalname . "') end) from purchaseanimal where name='" . $animalname . "' and zooname='" . $zooname . "') where zooname='" . $zooname . "' and name='" . $animalname . "'";
+                $cashquery = "update zoo set cash=(select cash from zoo where name='" . $zooname . "')-250 where name='" . $zooname . "'";
+                executePlainSQL($itemquery);
+                executePlainSQL($aimalquery);
+                executePlainSQL($cashquery);
                 OCICommit($db_conn);
-            }// Hydration +20, Hygiene -20, Fullness -5, Happiness +20
+            }// Hydration 0, Hygiene 20, Fullness 0, Happiness -10, Price 150
             if (array_key_exists('AxeBodyspray', $_POST)) {
-                $query = "update purchaseitem set amount=(select amount from purchaseitem where zooname='" . $zooname . "' and name='AxeBodyspray')-1 where zooname='" . $zooname ."' and name='AxeBodyspray'";
-                executePlainSQL($query);
+                $itemquery = "update purchaseitem set amount=(select amount from purchaseitem where zooname='" . $zooname . "' and name='AxeBodyspray')-1 where zooname='" . $zooname ."' and name='AxeBodyspray'";
+                $animalquery = "update purchaseanimal set hygiene=(select max(case when hygiene<0 then 0 else (select hygiene+20 from purchaseanimal where zooname='" . $zooname . "' and name='" . $animalname . "') end) from purchaseanimal where name='" . $animalname . "' and zooname='" . $zooname . "'), happiness=(select max(case when happiness<0 then 0 else (select happiness-10 from purchaseanimal where zooname='" . $zooname . "' and name='" . $animalname . "') end) from purchaseanimal where name='" . $animalname . "' and zooname='" . $zooname . "') where zooname='" . $zooname . "' and name='" . $animalname . "'";
+                $cashquery = "update zoo set cash=(select cash from zoo where name='" . $zooname . "')-150 where name='" . $zooname . "'";
+                executePlainSQL($itemquery);
+                executePlainSQL($animalquery);
+                executePlainSQL($cashquery);
                 OCICommit($db_conn);
-            }// Hydration 0, Hygiene 20, Fullness 0, Happiness -10
+            }
             
             if ($_POST && $success) {
                 header("location: animal.php");
