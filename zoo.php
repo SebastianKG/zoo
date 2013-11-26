560f9cbb6c9aa4181e486ef20f232a07a69bcd71
Sebastian Kazenbroot-Guppy <sebastian.kg@gmail.com>
2013-11-26 01:31:19 -0800

    Removed comments

--- zoo.php
+++ zoo.php
@@ -109,22 +109,6 @@
 					   }
             	}
 
-            	/*CREATE TABLE PurchaseAnimal (
-	name 				VARCHAR(32),
-	type				VARCHAR(32),
-	hydration			INTEGER,
-	fullness			INTEGER,
-	hygiene				INTEGER,
-	happiness 			INTEGER,
-	bodysize			INTEGER,
-	pen_id				INTEGER NOT NULL,
-	zooname				VARCHAR(32),
-	PRIMARY KEY (name, zooname),
-	FOREIGN KEY (pen_id,zooname) REFERENCES PurchasePen(id,zooname),
-	FOREIGN KEY (zooname) REFERENCES Zoo(name) 
-		ON DELETE CASCADE
-);*/
-
             	if ($_POST && $success) {
             		header('location: zoo.php');
             		die();
